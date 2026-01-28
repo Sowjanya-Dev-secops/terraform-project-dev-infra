b37967f7c5f1343aa7fd320417532d9eae37c4ed
Sowjanya-Dev-secops <sowji.muppala@gmail.com>
2026-01-28 16:54:32 +0530

    terraform_modules

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -3,7 +3,7 @@ module "vpc" {
     source = "git::https://github.com/Sowjanya-Dev-secops/terraform-aws-vpc.git?ref=main"
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
-    environment_name = var.environment_name
+    environment_name = var.environment
     vpc_tags = var.vps_tag
     public_subnet_cidrs=var.public_subnet_cidrs
     private_subnet_cidrs=var.private_subnet_cidrs
