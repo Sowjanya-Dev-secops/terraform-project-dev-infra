8432c4c963eac3bc12b6d334e7a1f1d12f31365f
Sowjanya-Dev-secops <sowji.muppala@gmail.com>
2026-01-30 21:46:45 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -50,7 +50,7 @@ resource "aws_instance" "redis" {
     }
   )
 }
-resource "terraform_data" "mongodb" {
+resource "terraform_data" "redis" {
   triggers_replace = [
     aws_instance.redis.id
   ]
@@ -76,9 +76,9 @@ resource "terraform_data" "mongodb" {
   }
 }
 
-resource "aws_instance" "redis" {
+resource "aws_instance" "rabbitmq" {
   ami           = local.ami_id
-  vpc_security_group_ids = [local.redis_sg_id]
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
   instance_type = "t3.micro"
   subnet_id = local.database_subnet_id
 
