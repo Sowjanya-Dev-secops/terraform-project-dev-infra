8318a622a3f85ea8cd3a32f640cf91a0c776a523
Sowjanya-Dev-secops <sowji.muppala@gmail.com>
2026-01-31 12:57:11 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 component=$1
 dnf install ansible -y
-# ansible-pull -U https://github.com/Sowjanya-Dev-secops/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+ansible-pull -U https://github.com/Sowjanya-Dev-secops/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 
-REPO_URL=https://github.com/Sowjanya-Dev-secops/ansible-roboshop-roles-tf.git
-REPO_DIR=/opt/roboshop/ansible
-ANSIBLE_DIR=ansible-roboshop-roles-tf
-mkdir -p $REPO_DIR
-mkdir -p /var/log/roboshop/
-cd $REPO_DIR
+# REPO_URL=https://github.com/Sowjanya-Dev-secops/ansible-roboshop-roles-tf.git
+# REPO_DIR=/opt/roboshop/ansible
+# ANSIBLE_DIR=ansible-roboshop-roles-tf
+# mkdir -p $REPO_DIR
+# mkdir -p /var/log/roboshop/
+# cd $REPO_DIR
 
-if [ -d $ANSIBLE_DIR ]; then
-    cd $ANSIBLE_DIR
-    git pull
-else
-    git clone $ANSIBLE_DIR
-    cd $ANSIBLE_DIR
+# if [ -d $ANSIBLE_DIR ]; then
+#     cd $ANSIBLE_DIR
+#     git pull
+# else
+#     git clone $ANSIBLE_DIR
+#     cd $ANSIBLE_DIR
 
-fi
+# fi
 
-ansible-playbook -e component=$component main.yaml
+# ansible-playbook -e component=$component main.yaml
