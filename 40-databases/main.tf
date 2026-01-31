43bdc3543fc60870d92d93eef8d9ddd480ccd289
Sowjanya-Dev-secops <sowji.muppala@gmail.com>
2026-01-31 12:29:52 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -31,8 +31,8 @@ resource "terraform_data" "mongodb" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-         "sudo sh /tmp/bootstrap.sh"
-        #"sudo sh /tmp/bootstrap.sh mongodb"
+         #"sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh mongodb"
     ]
   }
 }
@@ -71,7 +71,7 @@ resource "terraform_data" "redis" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
          # "sudo sh /tmp/bootstrap.sh"
-        "sudo sh /tmp/bootstrap.sh mongodb"
+        "sudo sh /tmp/bootstrap.sh redis"
     ]
   }
 }
