caa6640ec2f903fd4c9926ec311b5d5f180f78cb
Sowjanya-Dev-secops <sowji.muppala@gmail.com>
2026-02-01 18:29:38 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -17,7 +17,7 @@ resource "aws_lb" "backend_alb" {
   
 }
 #backend alb listening on port nmber 80
-resource "aws_lb_listener" "backend_end" {
+resource "aws_lb_listener" "backend_alb" {
   load_balancer_arn = aws_lb.backend_alb.arn
   port              = "80"
   protocol          = "HTTP"
